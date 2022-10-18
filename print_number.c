b8257c3b86c617b828e7f53c01954f1046f38546
Lola-boye <103613554+Lola-boye@users.noreply.github.com>
2022-10-18 10:05:10 +0100

    Update print_number.c

--- print_number.c
+++ print_number.c
@@ -3,7 +3,7 @@
 /**
  * print_unsigned_number - print unsigned number
  * @n: unsigned integer to print
- * Return: number of printed chars
+ * Return: number of printed characters
  */
 
 int print_unsigned_number(unsigned int n)
